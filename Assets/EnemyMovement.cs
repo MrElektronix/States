ad12c8a8c8f6b0f6926b7374b63a025f7cb530f3
Bram van der Kuip <vdkuipb@gmail.com>
2017-02-03 09:59:04 +0100

    Commit

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private NavMeshAgent _navMeshAgent;
+    private Vector3 _wanderPosition;
+    private bool _wander = true;
+
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _wanderPosition = new Vector3(0,0,0);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Wander());
+    }
+
+    private IEnumerator Wander()
+    {
+        while (_wander)
+        {
+            var randomX = Random.Range(-4, 4);
+            var randomY = Random.Range(-4, 4);
+
+            _wanderPosition = new Vector3(randomX, 0, randomY);
+
+            _navMeshAgent.SetDestination(_wanderPosition);
+            yield return new WaitForSeconds(3);
+        }
+    }
+}
