9ff3b86e2b524ac5bc803ce2a207b38b554920f5
MrElektronix <milovanpelt@gmail.com>
2017-03-10 09:51:14 +0100

    InRange

--- Assets/Milo/Range.cs
+++ Assets/Milo/Range.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Range : MonoBehaviour {
+public class Range : MonoBehaviour
+{
+   [SerializeField] private Transform _other;
+    private float _targetRadius;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private LayerMask _layerMask;
+
+
+    private void InRange()
+    {
+        if (!_other) return;
+        var distance = Vector3.Distance(_other.position, transform.position);
+        print("Distance to other object is: " + distance);
+    }
+
+    private void Update()
+    {
+        InRange();
+    }
 }
